d7cfc5cc065820acc83cfdf66ec6067bc358d426
RimshaMukhtar <rimshamukhtar747@gmail.com>
2024-04-04 02:17:07 +0500

    Adding a Todo List Application

---
+++ main.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let conditions = true;
+console.log(chalk.magenta.bold("\n \tWelcome to Rimsha's Todo-List Application\n"));
+while (conditions) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.green("Enter your New Task :")
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task added in Todo-List successfully`);
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more task ?",
+            default: "False"
+        }
+    ]);
+    conditions = addMoreTask.addmore;
+}
+console.log("Your updated Todo-list:", todoList);
